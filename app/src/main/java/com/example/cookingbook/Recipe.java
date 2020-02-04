149fae52ea3eece6453581b692c5a65499b308f4
ThugRzz <49206638+ThugRzz@users.noreply.github.com>
2020-02-04 19:51:30 +0300

    test

--- app/src/main/java/com/example/cookingbook/Recipe.java
+++ app/src/main/java/com/example/cookingbook/Recipe.java
@@ -5,34 +5,43 @@ public class Recipe {
     private String description;
     private String composition;
     private int image;
-    public Recipe(String title, String composition,String description,int image){
-        this.title=title;
-        this.composition=composition;
-        this.description=description;
-        this.image=image;
+
+    public Recipe(String title, String composition, String description, int image) {
+        this.title = title;
+        this.composition = composition;
+        this.description = description;
+        this.image = image;
     }
-    public void setTitle(String title){
-        this.title=title;
+
+    public void setTitle(String title) {
+        this.title = title;
     }
-    public void setDescription(String description){
-        this.description=description;
+
+    public void setDescription(String description) {
+        this.description = description;
     }
-    public void setComposition(String composition){
-        this.composition=composition;
+
+    public void setComposition(String composition) {
+        this.composition = composition;
     }
-    public void setImage(int image){
-        this.image=image;
+
+    public void setImage(int image) {
+        this.image = image;
     }
-    public String getTitle(){
+
+    public String getTitle() {
         return this.title;
     }
-    public String getDescription(){
+
+    public String getDescription() {
         return this.description;
     }
-    public String getComposition(){
+
+    public String getComposition() {
         return this.composition;
     }
-    public int getImage(){
+
+    public int getImage() {
         return this.image;
     }
 }
