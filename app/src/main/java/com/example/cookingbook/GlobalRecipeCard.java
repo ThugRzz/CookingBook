cc6bddd24e41bd7fb157b73bc25dab44bed8f941
ThugRzz <49206638+ThugRzz@users.noreply.github.com>
2020-02-24 15:54:52 +0300

    onclicklistener added, function "share" added

---
+++ app/src/main/java/com/example/cookingbook/GlobalRecipeCard.java
@@ -0,0 +1,14 @@
+package com.example.cookingbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GlobalRecipeCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_global_recipe_card);
+    }
+}
